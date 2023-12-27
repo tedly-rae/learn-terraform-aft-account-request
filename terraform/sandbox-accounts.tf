71ac112a8ea211f6d209c26c35f4444e6c2fe8dc
Tan Ly <tientan0912@gmail.com>
2023-12-27 14:50:07 +0700

    create a new sandbox account

--- terraform/sandbox-accounts.tf
+++ terraform/sandbox-accounts.tf
@@ -29,3 +29,35 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "sandbox02" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "tientan912+sandbox02@gmail.com"
+    AccountName               = "sandbox-account-02"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "tientan912+sandbox02@gmail.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT 02"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+    "Owner" = "tientan912@gmail.com"
+    "Division" = "IT"
+    "CostCenter" = "4988"
+    "Project" = "1126"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
